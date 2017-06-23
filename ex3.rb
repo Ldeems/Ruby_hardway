f0f3607ade826dbed70aa272c036b433c1c09457
Ldeems <luke.deems@yahoo.com>
2017-06-23 12:31:02 -0400

    ex3 math terms

---
+++ ex3.rb
@@ -0,0 +1,24 @@
+puts "i will now count my chickens:"
+
+puts "Hens #{25.+ 30}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7"
+
+puts 3 + 2 < 5 - 2
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "what is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's whay it's false."
+
+puts "How about some more."
+
+puts "Is it geater? #{5 > -2}"
+puts "Is it greater or equal? #{5 <= -2}"
+puts " is it less or equal? #{5 <= -2}"
+
